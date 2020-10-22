83eaef58c9e89691370c27fd518a79e2c336a828
requinard <david@requinard.nl>
2020-10-22 12:32:00 +0200

    rename int class

--- ints.py
+++ ints.py
@@ -1,22 +1,28 @@
-class InvertedInt:
+class GreedyInt:
     def __init__(self, i):
         self.i = i
 
     def __sub__(self, other):
+        """
+        This int is sneaky. When you try to subtract, it will instead add. All results will also be greedy.
+        """
         if isinstance(other, self.__class__):
-            return InvertedInt(self.i + other.i)
+            return GreedyInt(self.i + other.i)
         elif isinstance(other, int):
-            return InvertedInt(self.i + other)
+            return GreedyInt(self.i + other)
 
         raise ValueError("I don't know how to subtract this")
 
     def __str__(self):
         return str(self.i)
 
+    def __repr__(self):
+        return self.__str__()
+
 
 if __name__ == '__main__':
-    a = InvertedInt(1)
-    b = InvertedInt(1)
+    a = GreedyInt(1)
+    b = GreedyInt(1)
 
     print(a)  # should print
     print(b)  # should print 1
